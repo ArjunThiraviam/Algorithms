9463ccc5d0f58247ba8514595cfcb48d8ed868a0
athiraviam1 <athiraviam1@lenovo.com>
2019-11-03 22:07:09 +0530

    Annagram - using frequency counter

--- Colt Steele/Anagrams/script.js
+++ Colt Steele/Anagrams/script.js
@@ -1,9 +1,29 @@
-function checkAnagram(arr1, arr2) {
-    arr1
+function checkAnagram(first, second) {
+    first = first.split("");
+    second = second.split("");
+    if(first.length != second.length) {
+        return false;
+    }
 
+    let lookup = {};
 
+    for(let i in first ) {
+        let letter = first[i];
+        lookup[letter] ? lookup[letter] +=1 : lookup[letter]  = 1;
+    }  
 
+    console.log(lookup);
 
+    for(let key in second) {
+        let letter = second[key];
+        if(!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    
+    return true;
 }
 
-checkAnagram("car", "arc");
+checkAnagram("iceman", "cinema");
