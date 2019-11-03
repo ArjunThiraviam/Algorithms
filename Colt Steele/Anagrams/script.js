3883a24ca286c101b639e4e5de3e66db93b79b36
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-11-03 22:20:24 +0530

    Anagram - using frequency counter test

--- Colt Steele/Anagrams/script.js
+++ Colt Steele/Anagrams/script.js
@@ -7,10 +7,13 @@ function checkAnagram(first, second) {
 
     for(let i in first ) {
         let letter = first[i];
+        // if letter exist increment, otherwise set to 1
         lookup[letter] ? lookup[letter] +=1 : lookup[letter]  = 1;
     }  
+
     for(let key in second) {
         let letter = second[key];
+        // Can't find letter or letter is zero then it's not an anagram
         if(!lookup[letter]) {
             return false;
         } else {
